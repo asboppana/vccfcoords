7bcdfc7491ccc45fb822f1ff481d0fc0f26f36a5
asboppana <76268792+asboppana@users.noreply.github.com>
2023-08-07 20:29:42 +0000

    Adding solver functions to simulation object

--- vccfcoords/simulation.py
+++ vccfcoords/simulation.py
@@ -1,7 +1,9 @@
 import numpy as np
 from scipy.spatial import distance
 from vccfcoords import solvers
-from typing import Dict, Any, Tuple, List, Union
+import numpy.typing as npt
+import itertools
+from typing import Dict, Any, Tuple, List, Union, Set
 
 def get_euclid_dist(pt1:Union[Tuple[float, float, float], Tuple[float,float]], 
                     pt2:Union[Tuple[float, float, float], Tuple[float,float]]) -> float:
@@ -13,26 +15,6 @@ def get_euclid_dist(pt1:Union[Tuple[float, float, float], Tuple[float,float]],
     """
     
     return distance.euclidean(pt1, pt2)
-
-def get_closest_points(coords: np.ndarray, 
-                       target: Union[Tuple[float, float, float], Tuple[float,float]],
-                       n: int) -> Tuple[List[int], List[int]]:
-    """
-    Get the n closest points given a target point
-    :param coords: the pool of points to search
-    :param target: the coordinates of the target point 
-    :param n: the number of closest points to find
-    :return: the indices of the n closest points and the distances to those points
-    """
-
-    DIST_IDX = 0
-
-    distances = distance.cdist([target], coords, metric='euclidean')[DIST_IDX]
-
-    closest_idx = list(np.argsort(distances)[:n])
-    closest_distances = list(distances[closest_idx])
-
-    return closest_idx, closest_distances
 
 class Simulation:
     """
@@ -48,7 +30,7 @@ class Simulation:
                  coord_min: float,
                  coord_max: float,
                  n_ref: int,
-                 ref_strat: str,
+                 solver_type: str,
                  seed: int=0):
         
         """
@@ -58,34 +40,47 @@ class Simulation:
         :param coord_min: minimum value of the simulated coordinates
         :param coord_max: maximum value of the simulated coordinates
         :param n_ref: number of endothelial reference cells with known coordinates for each other cell of unknown coordinates
-        :param ref_strat: strategy for how the reference cells are selected ('random' or 'closest')
+        :param solver_type: value that specifies whether to use a geometric solver or an optimization solver (options 'geom' or 'opt')
         :param seed: value that ensures that if all other parameters of the simulation are the same, 
                      then simulations with the same seed value will contain the same simulated points
         """
+
+        #Setting Instance Varibales
         self.n_EC = n_EC
         self.n_OC = n_OC
         self.n_dim = n_dim
         self.coord_min = coord_min
         self.coord_max = coord_max
         self.n_ref = n_ref
-        self.ref_strat = ref_strat
+        self.solver_type = solver_type
         self.seed = seed
-
-
+     
         self.n_all = self.n_EC + self.n_OC
 
+        #Setting Seed for reproducibility
         np.random.seed(seed)
 
-        self.coords = self.sim_coordinates(self.n_all,
-                                           self.n_dim,
-                                           self.coord_min,
-                                           self.coord_max)
+        #Simulating Coordinates
+        self.coords = Simulation.sim_coordinates(self.n_all,
+                                                 self.n_dim,
+                                                 self.coord_min,
+                                                 self.coord_max)
         
-        self.EC_idx = set(np.random.choice(list(range(self.n_all)), self.n_EC, replace = False))
-        self.OC_idx = set(list(range(self.n_all))) - self.EC_idx
+        #Setting EC and OC indices 
+        self.EC_idx = list(set(np.random.choice(list(range(self.n_all)), self.n_EC, replace = False)))
+        self.OC_idx = list(set(list(range(self.n_all))) - set(self.EC_idx))
 
-    def sim_coordinates(self,
-                        n: int, 
+        #Generating Closest Points Dictionary
+        self.closest_points_dict = self.gen_closest_points_dict()
+
+        #Estimating OC coordinates
+        self.OC_coords_pred = self.solve()
+
+        #Computing Metrics
+        self.avg_cell_dist = self.get_avg_cell_dist()
+
+    @staticmethod
+    def sim_coordinates(n: int, 
                         n_dim: int, 
                         min_val: float=0,
                         max_val: float=1) -> np.ndarray:
@@ -103,14 +98,97 @@ class Simulation:
         
         return generated_coordinates
 
-    def get_closest(self,
-                    n_ref: int,
-                    ref_strat: str):
-        """
-        A method to modify attribute2.
-
-        Args:
-            new_text (str): The new string value to set attribute2.
-        """
+    @staticmethod
+    def get_closest_points(coordinate_pool: np.ndarray,
+                           coordinate_target: np.ndarray,
+                           n_closest: int) -> Tuple[List[int], List[float]]:
         
-        return
+        """
+        Get the n closest points given a target point
+        :param coordinate_pool: the pool of points to search
+        :param coordinate_target: the coordinates of the target point 
+        :param n_closest: the number of closest points to find
+        :return: the indices of the n closest points and the distances to those points
+        """
+
+        # Calculate distances from target coordinate to each point in the pool
+        distances = np.linalg.norm(coordinate_pool - coordinate_target, axis=1)
+
+        # Sort indices based on distances and get the n closest points
+        closest_indices = np.argsort(distances)[:n_closest]
+        closest_distances = list(distances[closest_indices])
+
+        return closest_distances, closest_indices
+
+    def gen_closest_points_dict(self) -> Dict[int, Tuple[List[int], List[float]]]:
+
+        """
+        Get the n closest points and distances from a list of target points
+        return: dictionary of OC indices as keys and tuples of closest EC indices and distance lists as values
+        """
+
+        #EC coordinate pool
+        pool_coords = self.coords[self.EC_idx]
+
+        #Dictionary to convert sequential indices to EC indices
+        convert_idx_dict = dict(zip(list(range(self.n_EC)), self.EC_idx))
+
+        closest_points_dict = dict()
+
+        #Generating closest point indices and distances for each OC cell
+        for idx in self.OC_idx:
+            target_coord = self.coords[idx]
+            closest_distances, closest_points_idx = Simulation.get_closest_points(pool_coords, 
+                                                                                  target_coord, 
+                                                                                  self.n_ref)
+            
+            closest_points_idx_converted = [convert_idx_dict[x] for x in closest_points_idx]
+            
+            closest_points_dict[idx] = (closest_distances, closest_points_idx_converted)
+
+        return closest_points_dict
+    
+    def solve(self) -> Dict[int, np.ndarray]:
+
+        """
+        Applies a 2D or 3D, geometric or optimization solver function to the tri/multi-lateration problem.
+        Specifically, this function computes or estimates the OC coordinates given distances to EC coordinates.
+        return: dictionary of OC indices as keys and a numpy array of estimated coordinates as values.
+        """
+
+        solver_func = None
+        DIST_IDX = 0
+        POINTS_IDX = 1
+
+        if self.n_dim == 2 and self.solver_type == "geom":
+            solver_func = solvers.geom_2D_solver
+        if self.n_dim == 2 and self.solver_type == "opt":
+            solver_func = solvers.opt_lse_2D_solver
+        if self.n_dim == 3 and self.solver_type == "geom":
+            solver_func = solvers.geom_3D_solver
+        if self.n_dim == 3 and self.solver_type == "opt":
+            solver_func = solvers.opt_lse_3D_solver
+
+        estimated_coords_dict = {x: np.asarray(solver_func(y[DIST_IDX], 
+                                                self.coords[y[POINTS_IDX]]))
+                                                for x,y in self.closest_points_dict.items()}
+
+        return estimated_coords_dict 
+    
+    def get_avg_cell_dist(self):
+
+        return [np.linalg.norm(x[0]-x[1]) for x in itertools.combinations(self.coords, 2)]
+        
+
+
+
+        
+
+
+         
+
+       
+
+            
+
+
